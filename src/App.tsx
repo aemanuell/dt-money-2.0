2036aa0fc997d57ef877022af123e716807f9e5e
aemanuell <antonioemanueell@gmail.com>
2022-10-24 16:17:14 -0300

    inicial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/defalt";
+export function App() {
+  return( 
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <Transactions/>
+    </ThemeProvider>
+  )
+}
